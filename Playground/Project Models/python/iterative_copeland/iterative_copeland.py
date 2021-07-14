da8fffccf617f8a42bc2bdfcf82c7dc5e1fdef51
sid <2482327b@student.gla.ac.uk>
2021-07-14 10:43:36 +0530

    final working iterative copeland method

--- Playground/Project Models/python/iterative_copeland/iterative_copeland.py
+++ Playground/Project Models/python/iterative_copeland/iterative_copeland.py
@@ -29,6 +29,9 @@ import math
 VOTERS = 5
 CANDIDATES = 4
 
+# Global value to remember the index location of each iteration.
+OFFSET = 0
+
 #                     V_0 V_1 V_2 V_3 V_4   <-- voters/agents
 original_rankings = [[0,  0,  3,  3,  1],   # C_0
                      [1,  1,  0,  0,  2],   # C_1
@@ -53,41 +56,76 @@ def pairwiseScoreCalcListFull(pref_profile, no_of_candidates):
 
 '''
     - Similar to the code above.
-    - Difference being that it doesn't recompute all scores, just appends to the scores list.
+    - Difference being that it doesn't recompute all scores, just appends to the 'scores list'.
     - NOTE: Needs to be changed to use the global scores array rather than a local copy.
 '''
-def pairwiseScoreCalcListNew(scores, pref_profile, no_of_candidates):
+def pairwiseScoreCalcListNew(pref_profile, no_of_candidates):
+    new_scores = []
     for j in range(no_of_candidates):
-        scores.append(sum([pref_profile[no_of_candidates][k] < pref_profile[j][k] for k in range(VOTERS)]))
-    return scores
+        new_scores.append(sum([pref_profile[no_of_candidates][k] < pref_profile[j][k] for k in range(VOTERS)]))
+    return new_scores
 
 def copelandScoreFull(scores, no_of_agents, no_of_candidates):
     final_score = [0]*no_of_candidates
-    for x,i in enumerate(scores):
-        return None        
+    for x, i in enumerate(scores):
+        r, c = list2matrix(x)
+        if i > no_of_agents//2:
+            final_score[r] += 1
+        elif i == no_of_agents/2:
+            final_score[r] += 0.5
+            final_score[c] += 0.5
+        else:
+            final_score[c] += 1
+
+    return final_score
+'''
+    - new_scores are the scores of the addition of a new candidate(the pairwise scores).
+    - final_score are the final copeland scores.
+    - no_of_agents is the number of agents that rank the candidates.
+    - offset is the 'virtual' pointer of the array position.
+'''
+def copelandScoreNew(new_scores, final_score, no_of_agents):
+    i_final_scores.append(0)
+    # imitating a pointer
+    global OFFSET
+    for i in new_scores:
+        r, c = list2matrix(OFFSET)
+        OFFSET += 1
+        if i > no_of_agents/2:
+            final_score[r] += 1
+        elif i == no_of_agents/2:
+            final_score[r] += 0.5
+            final_score[c] += 0.5
+        else:
+            final_score[c] += 1
+    return final_score
+
 '''
 - Reads list of scores and presents the scores[r][c] value
 '''
+
 def matrix2list(r, c, scores_list, no_of_voters):
-    if r < c: return no_of_voters - scores_list[math.ceil((c*(c-1)/2) + r)]
+    if r < c: return no_of_voters - scores_list[(c*(c-1)//2 + r)]
     elif r == c: return 0
-    else: return scores_list[math.ceil((r*(r-1)/2) + c)] # NOTE: change to roof/floor respectively. Not appropriately tested
-'''
-def list2matrix(k, scores_list):
-    # r < c
-    # 2*i = (r**2 - r + c)
-    2*k = r**2 - r + 2*c
-    r = 
-    c = 
-    i = n - 2 - math.floor(math.sqrt(-8*k + 4*n*(n-1)-7)/2.0 - 0.5)
-    j = k + i + 1 - n*(n-1)/2 + (n-i)*((n-i)-1)/2
-    # this tells us that RHS is even.
-    return (i,j)
-'''
+    else: return scores_list[(r*(r-1)//2 + c)] # NOTE: change to roof/floor respectively. Not appropriately tested
+
+def list2matrix(k):
+    # NOTE: Very bad implementation, could possibly just be an equation.
+    # It works though!!
+    r = 0
+    while r*(r-1)/2 <= k:
+        r += 1
+    r = r - 1
+    c = k - r*(r-1)//2
+    return (r,c)
+
 scores = pairwiseScoreCalcListFull(original_rankings, CANDIDATES)
+final_scores = copelandScoreFull(scores, VOTERS, CANDIDATES)
+print(scores)
+print(final_scores)
 
 '''
-    - Prints complete score matrix
+    - Prints complete pairwise score matrix
 '''
 def fullScoreMatrixOutput():
     for i in range(CANDIDATES):
@@ -95,8 +133,8 @@ def fullScoreMatrixOutput():
         for j in range(CANDIDATES):
             s = s + str(matrix2list(i, j, scores, VOTERS)) + " "
         print(s)
+# fullScoreMatrixOutput()
 
-fullScoreMatrixOutput()
 '''
 - Compares all preference profiles with each other.
 - Returns a CANDIDATE X CANDIDATE matrix.
@@ -119,13 +157,15 @@ def copelandScore(pref_profile, agents, voters):
         copeland_score.append(scoreCalc(i))
     return copeland_score
 
-'''
 # Grows with each addition to the candidates, samples from the original profile.
 i_rankings = []
 i_scores = []
+i_final_scores = []
 # Adds new agents, one at a time.
 for x, i in enumerate(original_rankings):
-    i_rankings.append(i)
-    i_scores = pairwiseScoreCalcListNew(i_scores, i_rankings, x)
-    print(i_scores)
-'''
+    i_rankings.append(i) # Addition of a new candidate from the original profile.
+    i_scores = pairwiseScoreCalcListNew(i_rankings, x)
+    i_final_scores = copelandScoreNew(i_scores, i_final_scores, VOTERS)
+    # print("Pairwise Scores: " + str(i_scores))
+    print("Final Score: " + str(i_final_scores))
+    
